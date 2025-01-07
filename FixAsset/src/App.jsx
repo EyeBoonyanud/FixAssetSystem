c1f26964e5d25f25956cd54c029b2771edd1a050
eyeya <eyephantana2001@hotmail.com>
2025-01-07 09:12:44 +0700

    ok eye

--- FixAsset/src/App.jsx
+++ FixAsset/src/App.jsx
@@ -20,8 +20,8 @@ import PDF_download from './PDF_fam_master/PDF_design'
 import Close_ACC from './Transaction/Search'
 import axios from "axios";
 
-const backendURL = "http://10.17.70.212:5000";
-// const backendURL = "http://10.17.100.183:5001"
+// const backendURL = "http://10.17.70.212:5000";
+const backendURL = "http://10.17.100.183:5001"
 axios.defaults.baseURL = backendURL;
 
 const App = () => {
